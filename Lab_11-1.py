676b26fc5684583c2e122a124a6dd4cd6fa459c9
p23aelliff <p23aelliff@fairfieldprep.org>
2023-01-30 09:16:27 -0500

    lab 11-1

---
+++ Lab_11-1.py
@@ -0,0 +1,10 @@
+#author ate (1/20/23)
+
+#Dictionary variable
+row = {2:"Tanner Langis", 3:"Jack Rydzik", 5:"Preston Lyons", 7:"Vishnu Kumar", 8:"Brian Connolly", 10:"Richie Whitcomb", 13:"Ben Ackerman", 15:"Aidan Elliff", 17:"Nick Orellana-Gonzalez", 18:"Nathanial Fabricatore", 21:"Colin Sasser", 24:"Joe Marotta", 27:"Jan Salafia", 28:"Jack McHale", 30:"Will CLegg", 34:"Sean Salafia", 35:"Robbie Donahue", 36:"Ethan Forrest"}
+
+#print statement
+print(row)
+
+#update to dictionary list
+del row[15]
