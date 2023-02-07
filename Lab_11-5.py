0c725c0bcd3675adcf07d088a3ed9eb1c2c66518
p23aelliff <p23aelliff@fairfieldprep.org>
2023-02-07 12:32:49 -0500

    Lab 5

--- Lab_11-5.py
+++ Lab_11-5.py
@@ -1,9 +1,15 @@
 #author ate (2/3/23)
 
 #variables
-day_date = 2, 3, 4, 5, 6
-(weekdays) = day_date
-tuple(weekdays)
+day_date = ("2", "3", "4", "5", "6")
+days = ("mon", "tues", "wed", "thu", "fri")
+(date, day) = (day_date, days)
 
 #print statement
-print(weekdays)
+print(date)
+print(day)
+
+#contact_info = ("555-5555", "jsample@fairfieldprep.org")
+#(phone, email) = contact_info
+#print(phone)
+#print(email)
